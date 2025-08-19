e024e495fa04c364ebf396a7e72ede9d72fceb3d
DarkAngel <marcelo@darkblue.tv>
2025-08-19 20:33:27 -0300

    ActionList | object model

--- models/app_data.py
+++ models/app_data.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from models.dmx.dmx_canvas import DMXCanvas
 from models.fixtures.fixture_list import FixtureList
-from models.lighting.actionList import ActionList
+from models.lighting.action_list import ActionList
 from models.lighting.plan import Plan
 from models.song.song import Song
 
