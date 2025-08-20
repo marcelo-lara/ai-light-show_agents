32983657fb2e492dca783710d2cab2eb1e5e9311
DarkAngel <marcelo@darkblue.tv>
2025-08-19 22:07:12 -0300

    EffectTranslator | housekeep

--- agents/effect_tramslator/effect_translator.py
+++ agents/effect_tramslator/effect_translator.py
@@ -8,17 +8,10 @@ class EffectTranslator(Agent):
         self._model = model
         super().__init__(model=self._model)
 
-    def get_actions_reference(self):
-        actions_reference = {}
-        for fixture in self.app_data.fixtures:
-            for action in fixture.actions:
-                actions_reference[action.name] = action
-        return actions_reference
-
     def parse_plan_entry(self, plan_entry:PlanEntry):
         user_prompt = plan_entry.description
         beats_array = self.app_data.song.get_beats_array(plan_entry.start, plan_entry.end)
-        actions_reference = self.get_actions_reference()
+        actions_reference = {action.name: action for fixture in self.app_data.fixtures for action in fixture.actions}
         self.parse_context(
             beats=beats_array,
             actions_reference=actions_reference,
